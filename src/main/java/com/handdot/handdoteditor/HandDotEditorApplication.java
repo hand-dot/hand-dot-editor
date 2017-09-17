aef303e174b9d5659e7a5aca3d4b6b63d8c9bc7e
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-09-17 23:37:04 +0900

    1st commit

---
+++ src/main/java/com/handdot/handdoteditor/HandDotEditorApplication.java
@@ -0,0 +1,12 @@
+package com.handdot.handdoteditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandDotEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandDotEditorApplication.class, args);
+	}
+}
