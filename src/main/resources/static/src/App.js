469f0a9dbe47bd95475a0caeeed99f314dd1c76d
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-09-18 00:35:31 +0900

    tmp commit

--- src/main/resources/static/src/App.js
+++ src/main/resources/static/src/App.js
@@ -1,9 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import MonacoEditor from "react-monaco-editor";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      code: "// type your code..."
+    };
+  }
+  editorDidMount(editor, monaco) {
+    console.log("editorDidMount", editor);
+    editor.focus();
+  }
+  onChange(newValue, e) {
+    console.log("onChange", newValue, e);
+  }
   render() {
+    const code = this.state.code;
+    const options = {
+      url:
+        "https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.1/require.min.js",
+      paths: {
+        vs: "http://localhost:3000/vs"
+      },
+      selectOnLineNumbers: true
+    };
     return (
       <div className="App">
         <div className="App-header">
@@ -13,6 +36,16 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <MonacoEditor
+          width="800"
+          height="600"
+          language="javascript"
+          theme="vs-dark"
+          value={code}
+          options={options}
+          onChange={this.onChange}
+          editorDidMount={this.editorDidMount}
+        />
       </div>
     );
   }
