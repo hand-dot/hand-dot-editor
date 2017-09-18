662c3996f223c6400ac0c7a984cb5e38ba3e1186
hand-dot <kyouhei.fukuda0729@gmail.com>
2017-09-19 03:39:26 +0900

    beta release

--- src/main/java/com/handdot/handdoteditor/config/WebSocketConfig.java
+++ src/main/java/com/handdot/handdoteditor/config/WebSocketConfig.java
@@ -10,11 +10,11 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint(new String[] { "/whiteboad" }).withSockJS();
+		registry.addEndpoint(new String[] { "/ws" }).withSockJS();
 	}
 
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 		registry.setApplicationDestinationPrefixes(new String[] { "/app" });
-		registry.enableSimpleBroker(new String[] { "/topic", "/queue" });
+		registry.enableSimpleBroker(new String[] { "/topic" });
 	}
 }
--- src/main/java/com/handdot/handdoteditor/controller/MainController.java
+++ src/main/java/com/handdot/handdoteditor/controller/MainController.java
@@ -3,6 +3,8 @@ package com.handdot.handdoteditor.controller;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +13,26 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping({ "/" })
 public class MainController {
+
+	private String text = "console.log('Let`s Shere URL!!');";
+
 	@RequestMapping(method = { RequestMethod.GET })
 	String index(Model model) throws UnknownHostException {
 		String addres = InetAddress.getLocalHost().getHostAddress();
 		model.addAttribute("ip", addres);
 		return "index";
+	}
+
+	@SendTo({ "/topic/ws" })
+	@MessageMapping({ "/ws/get" })
+	public String getText() throws InterruptedException {
+		return this.text;
+	}
+
+	@SendTo({ "/topic/ws" })
+	@MessageMapping({ "/ws/set" })
+	public String setText(String text) throws InterruptedException {
+		this.text = text;
+		return this.text;
 	}
 }
